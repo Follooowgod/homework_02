f9a4948eab74c54c7d7f1dd65ce7d3733be16ba9
TSOI S. <follooowgod@gmail.com>
2025-11-05 15:55:41 +0300

    does hw_8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -56,7 +56,7 @@ const HW8 = () => {
     return (
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #8</div>
-            <div className={s2.hw}>
+            <div className={s2.hw_2}>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
                         <SuperButton
